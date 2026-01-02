e75d5beb8f7419669e53aeabc580a4840eb8e2ec
Ofek Doitch <Tech@WeedonAndScott.com>
2026-01-02 15:27:24 -0500

    upgrade dependencies

--- flake.nix
+++ flake.nix
@@ -1,19 +1,14 @@
 {
   description = "A Nix-flake-based Node.js development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
-      overlays = [
-        (final: prev: rec {
-          nodejs = prev.nodejs_latest;
-        })
-      ];
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs {
-          inherit overlays system; config = {
+          inherit system; config = {
           allowUnfree = true;
         };
         };
